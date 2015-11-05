7124f8283f65f2dd69f7493e6c89f8f581984930
guomitang <1249919493@qq.com>
2015-11-05 15:01:08 +0800

    Update pic.js

--- pic.js
+++ pic.js
@@ -1,2 +1,25 @@
-var name="nana";
-console.log(name);
+var ele = document.createElement('script'),
+    _head = document.getElementsByTagName('head')[0];
+     
+ele.type='text/javascript';
+ele.src = "forGaofei这个文件的路径地址";
+_head.appendChild(ele);  
+ 
+ 
+if (ele.readyState){ //IE 
+    ele.onreadystatechange = function(){
+        if (ele.readyState == "loaded" || ele.readyState == "complete"){ 
+            ele.onreadystatechange = null; 
+            doSomething();
+        } 
+    }; 
+} else { //Firefox, Safari, Chrome, Opera 
+    ele.onload = function(){
+        doSomething();
+    }; 
+}
+ 
+function doSomething(){
+ 
+   console.log("name");
+}
